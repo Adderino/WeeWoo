380c5cee0454b60b2556f848568cc63e3fade497
Adderino <jonssonadam1@gmail.com>
2018-11-30 13:26:29 +0100

    Add project files.

---
+++ WeeWoo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeeWoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pet p = new Pet();
+
+            Console.Write("Name your pet: ");
+            p.Name = Console.ReadLine();
+
+            Console.ReadKey();
+        }
+    }
+}
